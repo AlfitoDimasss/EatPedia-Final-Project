bd9a0732af596e5029a6ec6993d48494d06a03c1
AlfitoDimasss <dimasalfito89@gmail.com>
2023-05-25 10:39:10 +0700

    add section and decoration latest recipe page

--- src/scripts/views/pages/components/jumbotron.js
+++ src/scripts/views/pages/components/jumbotron.js
@@ -1,13 +1,15 @@
 const Jumbotron = () => `
-  <div class="jumbotron">
-    <div class="jumbotron__image-box">
-      <img src="./images/hero.jpg" alt="Hero Image" crossorigin="anonymous">
+  <section class="section">
+    <div class="jumbotron">
+      <div class="jumbotron__image-box">
+        <img src="./images/hero.jpg" alt="Hero Image" crossorigin="anonymous">
+      </div>
+      <div class="jumbotron__desc-container">
+        <h1>Welcome to EatPedia</h1>
+        <p>Temukan berbagai jenis resep makanan yang mungkin anda suka dan mulailah memasak.</p>
+        <a href="#/recipes">Selengkapnya</a>
+      </div>
     </div>
-    <div class="jumbotron__desc-container">
-      <h1>Welcome to EatPedia</h1>
-      <p>Temukan berbagai jenis resep makanan yang mungkin anda suka dan mulailah memasak.</p>
-      <a href="#/recipes">Selengkapnya</a>
-    </div>
-  </div>
+  </section>
 `;
 export default Jumbotron;
--- src/scripts/views/pages/components/latest-recipe.js
+++ src/scripts/views/pages/components/latest-recipe.js
@@ -1,9 +1,14 @@
 const LatestRecipe = () => `
-  <div class="latest-recipe">
-    <h1>Resep Terbaru</h1>
-    <p>Resep-resep masakan terbaru yang diperbaharui setiap harinya.</p>
-    <div class="latest-recipe__recipe-container" id="recipeContainer"></div>
-  </div>
+  <section class="section latest-recipe-section">
+    <div class="latest-recipe">
+      <h1>Resep Terbaru</h1>
+      <p>Resep-resep masakan terbaru yang diperbaharui setiap harinya.</p>
+      <div class="latest-recipe__recipe-container" id="recipeContainer"></div>
+      <a href="#/recipes" class="latest-recipe__btn-selengkapnya">Selengkapnya</a>
+    </div>
+    <img src="./images/honey.png" alt="Honey" crossorigin="anonymous" class="latest-recipe__decoration latest-recipe__decoration-1">
+    <img src="./images/peach.png" alt="Peach" crossorigin="anonymous" class="latest-recipe__decoration latest-recipe__decoration-2">
+  </section>
 `;
 
 export default LatestRecipe;
