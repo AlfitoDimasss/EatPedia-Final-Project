48baa86fca00c11b869f1de957716f8217c3dccc
Salsabila Nur Ruslanti <123548236+Salsabilan12@users.noreply.github.com>
2023-05-27 13:52:33 +0700

    komponen feature
    menambahkan komponen fitur untuk section fitur kami

---
+++ src/scripts/views/pages/components/feature.js
@@ -0,0 +1,38 @@
+const Feature = () => `
+<section class="section">
+    <div class="feature">
+        <h1>Fitur Kami</h1>
+        <p>Berikut fitur yang tersedia pada website kami</p>
+        <div class="feature_container">
+            <div class="feature_container_card">
+                <i class="fa-regular fa-bookmark"></i>    
+                <h3>Bookmark</h3>
+                <p>Dengan fitur ini anda dapat menyimpan resep yang anda inginkan</p>
+            </div>
+            <div class="feature_container_card">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <h3>Search</h3>
+                <p>Anda dapat mencari resep yang anda inginkan dengan fitur ini</p>
+            </div>
+            <div class="feature_container_card">
+                <i class="fa-regular fa-newspaper"></i>
+                <h3>Article</h3>
+                <p>Fitur ini berisi artikel-artikel tentang makanan</p>
+            </div>
+            <div class="feature_container_card">
+                <i class="fa-solid fa-filter"></i>
+                <h3>Filter</h3>
+                <p>Anda dapat mencari resep sesuai kategori yang anda inginkan</p>
+            </div>
+        </div>
+    </div>
+    <img src="./images/round-bread.png" alt="Round Bread" class="feature__decoration feature__decoration-1">
+    <img src="./images/blueberry.png" alt="blueberry" class="feature__decoration feature__decoration-2">
+    <img src="./images/blueberry-1.png" alt="blueberry" class="feature__decoration feature__decoration-3">
+    <img src="./images/blueberry.png" alt="blueberry" class="feature__decoration feature__decoration-4">
+    <img src="./images/chicken.png" alt="chicken" class="feature__decoration feature__decoration-5">
+    <img src="./images/chicken-1.png" alt="chicken" class="feature__decoration feature__decoration-6">
+</section>
+`;
+
+export default Feature;
