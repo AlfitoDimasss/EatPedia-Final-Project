4445b0499e22a3a83f70816cbba3200cc1120e53
AlfitoDimasss <dimasalfito89@gmail.com>
2023-05-31 11:50:54 +0700

    update halaman resep

---
+++ src/scripts/views/pages/components/filter-card.js
@@ -0,0 +1,56 @@
+const FilterCard = () => `
+<div class="recipes__filter-wrapper">
+  <div class="recipes__filter">
+    <h5>Filter Resep</h5>
+    <form action="">
+      <label for="foodType">Tipe Masakan</label>
+      <select name="" id="foodType" class="recipes__filter__food-type">
+        <option value="">Semua Jenis</option>
+        <option value="">Breakfast</option>
+        <option value="">Appetizer</option>
+        <option value="">Drink</option>
+      </select>
+      <hr class="hr-form">
+      <label for="">Nutrisi</label>
+      <label for="kaloriMin" class="label-inner">Kalori</label>
+      <div class="recipes__filter__calori-wrapper">
+        <input type="text" placeholder="Min" id="kaloriMin" class="recipes__filter__calori">
+        <input type="text" placeholder="Max" id="kaloriMax" class="recipes__filter__calori">
+      </div>
+      <label for="kolestrolMin" class="label-inner">Kolestrol</label>
+      <div class="recipes__filter__calori-wrapper">
+        <input type="text" placeholder="Min" id="kolestrolMin" class="recipes__filter__calori">
+        <input type="text" placeholder="Max" id="kolestrolMax" class="recipes__filter__calori">
+      </div>
+      <label for="lemakMin" class="label-inner">Lemak</label>
+      <div class="recipes__filter__calori-wrapper">
+        <input type="text" placeholder="Min" id="lemakMin" class="recipes__filter__calori">
+        <input type="text" placeholder="Max" id="lemakMax" class="recipes__filter__calori">
+      </div>
+      <hr class="hr-form">
+      <label for="diet">Diet</label>
+      <div id="diet">
+        <input type="checkbox" id="vehicle1" value="Bike">
+        <label for="vehicle1" class="label-inner-checkbox"> Vegetarian</label><br>
+        <input type="checkbox" id="vehicle2" value="Car">
+        <label for="vehicle2" class="label-inner-checkbox"> Pascetarian</label><br>
+        <input type="checkbox" id="vehicle3" value="Boat">
+        <label for="vehicle3" class="label-inner-checkbox"> Gluten Free</label><br>
+      </div>
+      <hr class="hr-form">
+      <label for="diet">Alergi</label>
+      <div id="diet">
+        <input type="checkbox" id="vehicle1" value="Bike">
+        <label for="vehicle1" class="label-inner-checkbox"> Kacang</label><br>
+        <input type="checkbox" id="vehicle2" value="Car">
+        <label for="vehicle2" class="label-inner-checkbox"> Telur</label><br>
+        <input type="checkbox" id="vehicle3" value="Boat">
+        <label for="vehicle3" class="label-inner-checkbox"> Seafood</label><br>
+      </div>
+      <button type"button">Search</button>
+    </form>
+  </div>
+</div>
+`;
+
+export default FilterCard;
--- src/scripts/views/pages/recipes.js
+++ src/scripts/views/pages/recipes.js
@@ -1,12 +1,28 @@
+import FilterCard from './components/filter-card';
+
 const Recipes = {
   async render() {
     return `
-      <h2>Recipes Page</h2>
+      <div class="content" id="content"></div>
     `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const content = document.querySelector('.content');
+    content.innerHTML += `
+      <section class="section">
+        <p class="filter-button">Filter</p>
+        <div class="recipes-wrapper">
+          ${FilterCard()}
+        </div>
+      </section>
+    `;
+
+    const filter = document.querySelector('.filter-button');
+    filter.addEventListener('click', () => {
+      const filterCard = document.querySelector('.recipes__filter-wrapper');
+      filterCard.style.display = 'block';
+    });
   },
 };
 
