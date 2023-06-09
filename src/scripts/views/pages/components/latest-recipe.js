e0e06f62ba452afeebb7f49ce5938c5275aeb477
AlfitoDimasss <dimasalfito89@gmail.com>
2023-06-09 20:41:01 +0700

    styling halaman beranda

--- src/scripts/views/pages/components/latest-recipe.js
+++ src/scripts/views/pages/components/latest-recipe.js
@@ -2,7 +2,7 @@ const LatestRecipe = () => `
   <section class="section latest-recipe-section">
     <div class="latest-recipe">
       <h1>Latest Recipe</h1>
-      <p>New recipes that are updated daily.</p>
+      <p class="latest-recipe__description">New recipes that are updated daily.</p>
       <div class="latest-recipe__recipe-container" id="recipeContainer"></div>
       <a href="#/recipes" class="latest-recipe__btn-selengkapnya">See More</a>
     </div>
